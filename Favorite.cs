885d5c33eaeeda70cbd07933b9f639ce544beed7
camilafelix <camila_nfs@hotmail.com>
2020-11-14 13:49:31 -0800

    Add files via upload

---
+++ Favorite.cs
@@ -0,0 +1,11 @@
+public class Favorites
+{
+	public static void Main()
+	{
+		System.Console.Out.WriteLine("Camila.\n");
+		
+		System.Console.Out.WriteLine("Another Cinderella Story.\n");
+		
+		System.Console.Out.WriteLine("Pink.");
+	}
+}
